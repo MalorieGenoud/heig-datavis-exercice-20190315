eff615348fbece8a9c19a4783560cc7e6e94b6c0
Malo Senpai <malorie.genoud@gmail.com>
2019-03-28 16:04:04 +0100

    exercice node

--- calculateByCountry.js
+++ calculateByCountry.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch')
+const d3 = require('d3')
 const R = require('ramda')
 
 const writeJson = require('./writeJson')
@@ -5,9 +7,24 @@ const writeJson = require('./writeJson')
   writeJson prends deux arguments:
     * le nom du fichier
     * un objet ou une liste JSON
-  
+
   ex:
     writeJson('asylumByCountry.json', list)
 */
-const DATA = require('./asylum.json')
-// asylum.json doit être créé avec prepareData
+
+const DATA = require('./asylum.json') // asylum.json doit être créé avec prepareData
+
+const COUNTRIES = R.uniq(DATA.map(R.prop("country_asylum"))); // récupère tous les pays
+
+const findAndCountCountry = country_asylum => ({
+    country_asylum, somme :
+        DATA.filter(d => d.country_asylum === country_asylum) // pour filtrer les données par pays
+            .map(d => d.affected) // pour créer un tableau avec que les nombres de migrants
+            .reduce((somme, d) => somme + d, 0) // pour incrémenter les valeurs et avoir plus que la somme
+});
+
+let COUNTRIES_STATS = COUNTRIES.map(pays => findAndCountCountry(pays));
+
+writeJson('asylumByCountry.json', COUNTRIES_STATS); // pour créer le fichier
+
+console.log("JSON create")
--- prepareData.js
+++ prepareData.js
@@ -1,16 +1,38 @@
-const fetch = require('node-fetch')
-const d3 = require('d3')
-const R = require('ramda')
-
+const fetch = require('node-fetch');
+const d3 = require('d3');
+const R = require('ramda');
 const writeJson = require('./writeJson')
-/*
-  writeJson prends deux arguments:
-    * le nom du fichier
-    * un objet ou une liste JSON
-  
-  ex:
-    writeJson('asylum.json', data)
-*/
 
-const URL = 'https://raw.githubusercontent.com/idris-maps/heig-datavis-2019/master/20190315-node/exercie_node/ch_asylum_demands.csv'
-// le lien vers le fichier CSV
+const url = 'https://raw.githubusercontent.com/idris-maps/heig-datavis-2019/master/20190322-node/exercice_node/ch_asylum_demands.csv';
+
+const unknown = function(list){
+    list.filter(data => data.affected !== "*")
+    return list;
+};
+
+const yearInt = function(list){
+    list.map(data => ({...data, year: Number(data.year)}))
+    return list;
+};
+
+const affectedInt = function(list){
+    list.map(data => ({...data, affected: Number(data.affected)}))
+    return list;
+};
+
+const makeUSA = function (list) {
+    list.map(data => ({
+        ...data, country_asylum: data.country_asylum.includes("USA") ? "USA" : data.country_asylum
+    }))
+    return list;
+};
+
+fetch(url)
+    .then(data => data.text())
+    .then(csv => d3.csvParse(csv))
+    .then(list => unknown(list))
+    .then(list => yearInt(list))
+    .then(list => affectedInt(list))
+    .then(list => makeUSA(list))
+    .then(data => writeJson('asylum.json', data))
+    .then(console.log('JSON create'));
